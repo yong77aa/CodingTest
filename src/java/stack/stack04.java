c0bc6d7c853230a57e012f425445cf22129d8e02
DESKTOP-RVS3V6T\insun <dldlstjs9404@gmail.com>
2021-07-22 01:38:49 +0900

    [ADD] 20210721 스택4

---
+++ src/java/stack/stack04.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class stack04 {
+    public static void main(String[] args) throws IOException {
+    
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(bf.readLine()); // 몇개 받는지
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i<k; i++){
+         int n = Integer.parseInt(bf.readLine());
+            if(n == 0) {
+                //0
+                stack.pop();
+            }else{
+                stack.add(n);
+            }
+        }
+
+        int count = 0;
+        while(!stack.isEmpty()){
+            count += stack.pop();
+        }
+
+        System.out.println(count);
+    }
+}
