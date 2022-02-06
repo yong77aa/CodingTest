016390caf91bd9811ba5d97bc4e1d6cec8938978
Insun Lee <dldlstjs9404@gmail.com>
2022-02-06 16:57:05 +0900

    [CHANGE] 어제 문제 풀이 변경

--- src/test/warmingup/level2_8.js
+++ src/test/warmingup/level2_8.js
@@ -1,26 +1,19 @@
 function solution(numbers) {
     var answer = '';
-    //greedy 인거 같음..
-    //일단 1. 첫번째 인덱스에 있는 숫자가 가장 큰 애를 처음으로 세우자..
-    //2. 첫번째 인덱스 숫자가 같으면, 길이가 더 긴 숫자중에서 두번째 있는 숫자가 더 큰 애를 앞으로 세우자..
+    //greedy 인거 같음.. -> 아니고 정렬이었음..
+    //sort메서드 잘 알아보고 쓰기..
 
 
-    numbers.sort().reverse()
-    console.log(numbers)
-    
-    //0만 있을 때
-    let zeroCount = 0;    
-    answer = numbers.reduce((acc, v, i, arr) => {
-        if(v == 0) zeroCount++;
-    
-        if(String(v)[String(v).length - 1] == 0 && arr[i+1]){
-            v = arr[i+1]
-            arr[i+1] = arr[i]
-        }
-        return acc + v
-    }, "")
+    let newNumbers = numbers.map((v) => {
+        return v+""
+    }).sort().reverse().sort((a,b) => {
+        let tempA = a + b
+        let tempB = b + a
+        return parseInt(tempB) - parseInt(tempA)
+    })
+  
     
     
-    return answer.length != zeroCount ? answer : "0" ;
+    return newNumbers.join("")[0] == "0" ? "0" : newNumbers.join("");
     
 }
