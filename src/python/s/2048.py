eb6c201dd89632c742af9a7617e3e98c067018dd
YONG <yong77aa@naver.com>
2022-02-13 23:57:37 +0900

    2048 푸는중

--- src/python/s/2048.py
+++ src/python/s/2048.py
@@ -14,7 +14,41 @@ size = stdin.readline()
 board = [list(stdin.readline()) for _ in range(size)]
 
 # 이동 함수 (상, 하, 좌 우)
-def move():
+# 인덱스 위치를 이동하면서 값이 같은지 체크하고, 같으면 더하고 다르면 그냥 냅두기
+def move(board, direction):
+    if direction == '0':  # 상
+        for j in range(size):
+            top = 0
+            for i in range(1, size):
+                if board[i][j]:
+                    temp = board[i][j]  # 해당 값을 변수에 저장하고
+                    board[i][j] = 0  # 그 자리의 값을 0으로 변경
+
+                    if board[i][top] == 0:  # 이동하려는 자리의 값이 0이면
+                        board[i][top] = temp  # 이전 자리의 값을 넣음
+                    elif board[i][top] == temp:  # 이동하려는 자리의 값이 이전 자리의 값과 같은 경우
+                        board[i][top] = temp * 2  # 이전 자리의 값에서 곱하기 2한 값을 넣음
+                        top += 1
+                    else:  # 이동하려는 자리의 값이 이전 자리의 값과 다르고 0이 아닌 경우
+                        top += 1
+                        board[i][j] = temp  # 원래 자리에다가 다시 넣음
+
+    elif direction == '1':  # 하
+        for j in range(size):
+            top = size - 1  # 위에서 아래로 가는거니까 가장 큰 값에서 값을 하나씩 줄여나감
+            for i in range(size - 2, -1, -1):  # 시작범위, 마지막범위, 이동 값
+                temp = board[i][j]
+                board[i][j] = 0
+
+                if board[top][j] == 0:
+                    board[top][j] = temp
+                elif board[top][j] == temp:
+                    board[top][j] = temp * 2
+
+
+    elif direction == '2':  # 좌
+
+    elif direction == '3':  # 우
 
 
 # 이동 함수를 하나씩 호출하면서 모든 경우의 수를 탐색하는 함수 (재귀호출)
