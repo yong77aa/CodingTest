7268c44640b8143e271390664b8cfe45f7634bf4
Insun Lee <dldlstjs9404@gmail.com>
2022-03-03 20:23:57 +0900

    [CHANGE] 프린터 푸는 방법 바꿔서 다시품

--- src/test/warmingup/level2_12.js
+++ src/test/warmingup/level2_12.js
@@ -1,53 +1,39 @@
 function solution(priorities, location) {
     var answer = 0;
-    
-    let result = getPriorityMapAndLocationLetter(priorities, location) //Letter, 
-    
-    let mapWithLetterAndIndex = result[0]; // letter + index map
-    let locationLetter = result[1]; //location 에 해당하는 알파벳 글자
-    
-    //console.log('mapWithLetterAndIndex == ' + [...mapWithLetterAndIndex.keys()])
-    
-   let mapArray = [...mapWithLetterAndIndex.entries()]; //array로 반환 [알파벳, 중요도]
-    let testArray = [];
-    let max = ""
-    while(mapArray.length > 0){
-        //하나라도 남아있을 때는 이걸 해야함
-        //비교해야해..
-       let a = mapArray.shift(); //가장 앞에 있는 요소를 꺼냅니다.
-        if(getMax(mapArray)[0]){
-             max = getMax(mapArray)[0][1] //최대 값
+    let arr = makeArrayWithLocation(priorities) //2차원 배열로 location을 포함하여 만듦
+    let count = 0;
+    while(arr.length > 0){
+        //인쇄
+        const a = arr.shift();
+        const max = getMax(arr);
+        
+        if(a[1] >= max) {
+            count++; //출력되었음.
+        }else{
+            arr.push(a)
         }
-       if(a[1] >= max){
-           //이거보다 중요도가 높은것이 없을 때
-           testArray.push(a);
-       }else{
-           mapArray.push(a)
-       }
-    }
-    
-    console.log('testArray == ' + testArray);
-}
-
-function getPriorityMapAndLocationLetter(arr, location){
-    let newMap = new Map();
-    let locationLetter = "";
-    for(let i = 65; i < 65 + arr.length; i++){
-        let a = String.fromCharCode(i)
-        // newMap.set(a, arr[i-65])
-        newMap.set(a,arr[i-65])
-        if(location == i-65){
-            locationLetter = a
+        
+        if(a[0] == location) {
+            answer = count;
         }
     }
     
-    return [newMap, locationLetter];
+    return answer;
     
 }
 
-function getMax(arr){
-    arr.sort((a,b) => {
-        return b[1] - a[1]
+function makeArrayWithLocation(priorities){
+    let newArray = priorities.map((priority, index) => {
+        return [index, priority]
     })
-    return arr
+    return newArray;
+}
+
+
+function getMax(arr) {
+    let max = 0;
+    arr.forEach((item) => {
+        max = Math.max(max, item[1])
+    })
+    return max
 }
