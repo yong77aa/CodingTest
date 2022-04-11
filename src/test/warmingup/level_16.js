6f2af0b462ece6b43af7cfc7557965be9c28fc82
DESKTOP-RVS3V6T\insun <dldlstjs9404@gmail.com>
2022-04-12 06:54:02 +0900

    [ADD] 튜플완성

--- src/test/warmingup/level_16.js
+++ src/test/warmingup/level_16.js
@@ -1,29 +1,34 @@
 function solution(s) {
     let answer = [];
     let newArr = [];
-    let tempArr = []
-    const str = s.split(",").forEach((item, index) => { 
-        console.log(item)
-        // if(index != 0 && index != s.split("").length -1){
-        //     if(item != "{" && item != "}" && item != ","){
-        //         tempArr.push(item * 1)
-        //     }else if(item == "}"){
-        //         let newTempArr = [...tempArr]
-        //         newArr.push(newTempArr)
-        //         tempArr = [];
-        //     }
-        // }
+    
+    const str = s.split("{").forEach((item, index, arr) => { 
+        let tempArr = []
+        item = item.replace(/}/gi,"")
+        item.split(",").forEach((letter) => {
+            if(letter != "}" && letter != "," && letter != ""){
+                tempArr.push(parseInt(letter))
+            }
+        })
+        if(tempArr.length > 0) newArr.push(tempArr)
     })
+   console.log(newArr) //이중배열완성
+    
     
    const lengthArr = newArr.reduce((acc,item,index) => {
         return acc.concat(item.length)
    },[])
   
-    // newArr.forEach((item) => {
-    //     if(item.length === 1){
-    //         answer.shift(item)
-    //     }
-    // })
+    let newLengthArr = [...lengthArr]
+    newLengthArr.sort((a,b) => a-b).forEach((item) => {
+        const tempItem = newArr[lengthArr.indexOf(item)]
+        answer.forEach((num) => {
+            tempItem.splice(tempItem.indexOf(num),1)
+        })
+        tempItem.forEach((temp) =>{
+            answer.push(temp)
+        })
+    })
     
     return answer;
 }
