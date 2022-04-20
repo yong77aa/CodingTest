6d345f5b2a1c29a714eb8a4e48ec85a1842cd8c3
Insun Lee <dldlstjs9404@gmail.com>
2022-04-21 06:48:33 +0900

    [CHANGE] 방법 변경

--- src/test/warmingup/level2_19.js
+++ src/test/warmingup/level2_19.js
@@ -1,5 +1,4 @@
 function solution(info, query) {
-    var answer = [];
     const arr = [];
     const queryArr = query.map((item) => {
         item = item.replace(/and /g, '')
@@ -10,36 +9,22 @@ function solution(info, query) {
        return item.split(" ");
     })
     
-    const booleanArr = [];
-    
     queryArr.forEach((conditions) => {
-        
-        console.log('conditions == ' + conditions)
         let count = 0;
         tempArr.forEach((personItem, personIndex) => {
             let isSatisfied = true
-            personItem.forEach((personCondition, index) => {
-                console.log('conditions[index] == ' + conditions[index] + 'personCondition == ' + personCondition)
-                if(personItem.length -1 === index && isSatisfied){
-                    conditions[index] <= personCondition ? isSatisfied = true : isSatisfied = false
-                    console.log('conditions[index] <= personCondition ' + conditions[index] <= personCondition )
-                    //if(isSatisfied) count++;
-                }else if(isSatisfied && conditions[index] !== '-'){
+            personItem.toString().split(",").forEach((personCondition, index) => {
+                if(!isSatisfied) return
+                const personItemIndex = (personItem.toString().split(",").length -1)
+                if(personItemIndex == index){
+                    parseInt(conditions[index]) <= parseInt(personCondition) ? isSatisfied = true : isSatisfied = false
+                }else if(conditions[index] !== '-'){
                     personCondition === conditions[index] ? isSatisfied = true : isSatisfied = false
                 }
-                //console.log(conditions[index])
-                // conditions.forEach((condition, index) => {
-                // //조건 하나 씩
-                //     console.log('personConditioin == ' + personCondition + 'condition == ' + condition)
-                // })
             })
-            console.log('isSatisfied == ' + isSatisfied)
-           isSatisfied ? count++ : ''
-            
+            isSatisfied ? count++ : ''  
         })
-        
         arr.push(count)
     })
-    console.log(arr)
     return arr;
 }
