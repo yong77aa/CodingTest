85220b0cbe6e19c1b301959dcfc1fdb49128dbae
yong77aa <yong77aa@naver.com>
2022-04-26 23:00:21 +0900

    폴더 정리

---
+++ src/python/past/s/test.py
@@ -0,0 +1,4 @@
+print('%s, eggs, and %s' % ('spam', 'SPAM!'))
+print('{0}, eggs, and {1}'.format('spam', 'SPAM!'))
+print('{}, eggs, and {}'.format('spam', 'SPAM!'))
+print({spam}, eggs, and {SPAM!}'.format('spam', 'SPAM!'))
