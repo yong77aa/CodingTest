b8182f2a10d6022e8328f4d8e078d86bff411540
yong77aa <yong77aa@naver.com>
2022-04-27 01:39:44 +0900

    쉬운 계단 끝

--- src/python/new/easy_stair.py
+++ src/python/new/easy_stair.py
@@ -1,27 +1,17 @@
-def compare_value(arr):
-    global count
-    for i in arr:
-        myList = list(str(i))
-
-        result = True
-        for index, val in enumerate(myList):
-            if index < len(myList) - 1:
-                if int(myList[index + 1]) - int(myList[index]) != 1:
-                    result = False
-                    break
-        if result:
-            # 계단
-            count += 1
-
+def cal(n, arr):
+    for i in range(1, n):
+        for j in range(10):
+            if j == 0:
+                arr[i][j] = arr[i-1][1]
+            elif j == 9:
+                arr[i][j] = arr[i-1][8]
+            else:
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j+1]
 
 n = int(input())
-arr = []
-min = 10 ** (n-1)
-max = 10 ** (n) - 1
-count = 0
+arr = [[0] * 10 for _ in range(n)]
+for i in range(1, 10):
+    arr[0][i] = 1
 
-for i in range(min, max+1):
-    arr.append(i)
-
-compare_value(arr)
-print(str(count % 1000000000))
+cal(n, arr)
+print("result: " + str(sum(arr[n-1]) % 1000000000))
