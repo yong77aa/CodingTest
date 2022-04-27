1e0ab60a4982fc73012c1804072ac7bc7a5ee98b
YONG <yong77aa@naver.com>
2022-04-28 00:44:42 +0900

    1, 2, 3 더하기/ 쉬운 계단 수 다시풀었음

--- src/python/new/easy_stair.py
+++ src/python/new/easy_stair.py
@@ -1,4 +1,5 @@
-def cal(n, arr):
+def sol():
+    global n, arr
     for i in range(1, n):
         for j in range(10):
             if j == 0:
@@ -8,10 +9,11 @@ def cal(n, arr):
             else:
                 arr[i][j] = arr[i-1][j-1] + arr[i-1][j+1]
 
-n = int(input())
-arr = [[0] * 10 for _ in range(n)]
-for i in range(1, 10):
-    arr[0][i] = 1
-
-cal(n, arr)
-print("result: " + str(sum(arr[n-1]) % 1000000000))
+if __name__ == '__main__':
+    n = int(input())
+    arr = [[0] * 10 for _ in range(n)]
+    for i in range(1, 10):
+        arr[0][i] = 1
+    sol()
+    compare = 1000000000
+    print(sum(arr[n-1]) % compare)
