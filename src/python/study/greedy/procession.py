75a502cc6f5cdcfc5d639f7f9fc36cec7bb8489c
YONG <yong77aa@naver.com>
2022-05-12 00:58:04 +0900

    행렬 다시보기

--- src/python/study/greedy/procession.py
+++ src/python/study/greedy/procession.py
@@ -1,4 +1,5 @@
 # 백준알고리즘 https://www.acmicpc.net/problem/1080
+# 가장 첫번째 값부터 비교해나감
 
 n, m = map(int, input().split())
 a = []
@@ -13,6 +14,27 @@ for i in range(n):
     b.append(temp)
 
 
+def change(i, j):
+    for x in range(i, i+3):
+        for y in range(j, j+3):
+            if a[x][y] == 0:
+                a[x][y] = 1
+            else:
+                a[x][y] = 0
 
-print(a)
-print(b)
+
+result = 0
+if (n < 3 or m < 3) and a != b:
+    result = -1
+else:
+    for i in range(n-2):
+        for j in range(m-2):
+            if a[i][j] != b[i][j]:
+                result += 1
+                change(i, j)
+
+if result != -1:
+    if a != b:
+        result = -1
+
+print(result)
